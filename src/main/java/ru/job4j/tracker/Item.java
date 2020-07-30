b9db908283e21b6c87fd296344318b60fd7115f8
igorshirokov93 <freelancerigor@yandex.ru>
2020-07-30 12:38:36 +0300

    2.1. Реализация класса StartUI [toString в Item]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -25,4 +25,9 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return String.format("id: %s, name: %s", id, name);
+    }
 }
