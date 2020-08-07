3e42f0d70ff23c25f3af5e7d92de67c8c1e6ef71
igorshirokov93 <freelancerigor@yandex.ru>
2020-08-07 08:37:45 +0300

    1.1. Тесты на StartUI. [#223313]

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.tracker;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ValidateInputTest {
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"1", "0"});
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu.%n"
+                                + "0. Exit%n"
+                )
+        ));
+    }
+}
