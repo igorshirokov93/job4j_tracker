30a3819478de00122d2e6a43a8630406e628024d
igorshirokov93 <freelancerigor@yandex.ru>
2020-12-07 14:22:43 +0300

    1. Анонимные классы. [#214142]

---
+++ src/main/java/ru/job4j/lambda/Attachment.java
@@ -0,0 +1,33 @@
+package ru.job4j.lambda;
+
+/**
+ * @author Igor Shirokov (mailto:freelancerigor@yandex.ru)
+ * @version $1$
+ * @since 07.12.2020.
+ */
+
+public class Attachment {
+    private String name;
+    private int size;
+
+    public Attachment(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "name='" + name + '\'' +
+                ", size=" + size +
+                '}';
+    }
+}
