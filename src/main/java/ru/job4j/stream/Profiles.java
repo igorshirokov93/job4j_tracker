72e879b19a8e01ed793802f3032a744afa3c2766
igorshirokov93 <freelancerigor@yandex.ru>
2021-03-18 17:20:41 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,12 +1,13 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 /**
  * @author Igor Shirokov (mailto:freelancerigor@yandex.ru)
- * @version $1$
- * @since 15.03.2021.
+ * @version $2$
+ * @since 18.03.2021.
  */
 
 public class Profiles {
@@ -14,7 +15,8 @@ public class Profiles {
     List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
+                .sorted(Comparator.comparing(Address::getCity))
+                .distinct()
                 .collect(Collectors.toList());
-
     }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -3,25 +3,47 @@ package ru.job4j.stream;
 import org.junit.Test;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class ProfilesTest {
+
     @Test
-    public void register() {
-        Profiles func = new Profiles();
-        List<Address> expect = List.of(
+    public void sortedByCity() {
+        Profiles prof = new Profiles();
+        List<Address> addresses = prof.collect(List.of(
+                new Profile(new Address("Калининград", "Киевская", 82, 17)),
+                new Profile(new Address("Санкт-Петербург", "2-я Советская", 25, 9)),
+                new Profile(new Address("Москва", "Тверская", 4, 3)),
+                new Profile(new Address("London", "Park Avenue", 91, 69))
+        ));
+        List<Address> expected = List.of(
                 new Address("London", "Park Avenue", 91, 69),
+                new Address("Калининград", "Киевская", 82, 17),
                 new Address("Москва", "Тверская", 4, 3),
-                new Address("Санкт-Петербург", "2-я Советская", 25, 9),
-                new Address("Калининград", "Киевская", 82, 17)
+                new Address("Санкт-Петербург", "2-я Советская", 25, 9)
         );
-        List<Profile> input = expect.stream()
-                .map(Profile::new)
-                .collect(Collectors.toList());
-        List<Address> rsl = func.collect(input);
-        assertThat(rsl, is(expect));
+        assertThat(addresses, is(expected));
+    }
+
+    @Test
+    public void sortedByCityAndDistinct() {
+        Profiles prof = new Profiles();
+        List<Address> addresses = prof.collect(List.of(
+                new Profile(new Address("Москва", "Тверская", 4, 3)),
+                new Profile(new Address("Москва", "Тверская", 4, 3)),
+                new Profile(new Address("London", "Park Avenue", 91, 69)),
+                new Profile(new Address("Санкт-Петербург", "2-я Советская", 25, 9)),
+                new Profile(new Address("Санкт-Петербург", "2-я Советская", 25, 9)),
+                new Profile(new Address("Калининград", "Киевская", 82, 17))
+        ));
+        List<Address> expected = List.of(
+                new Address("London", "Park Avenue", 91, 69),
+                new Address("Калининград", "Киевская", 82, 17),
+                new Address("Москва", "Тверская", 4, 3),
+                new Address("Санкт-Петербург", "2-я Советская", 25, 9)
+        );
+        assertThat(addresses, is(expected));
     }
 }
