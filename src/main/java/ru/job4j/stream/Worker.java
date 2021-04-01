23244c6f89219f1941aa7937e4304e897470ce47
igorshirokov93 <freelancerigor@yandex.ru>
2021-04-01 20:07:15 +0300

    9. Шаблоны проектирования в Stream API [#310012]

---
+++ src/main/java/ru/job4j/stream/Worker.java
@@ -0,0 +1,93 @@
+package ru.job4j.stream;
+
+/**
+ * @author Igor Shirokov (mailto:freelancerigor@yandex.ru)
+ * @version $1$
+ * @since 01.04.2021.
+ */
+
+public class Worker {
+    private String name;
+
+    private String gender;
+
+    private byte age;
+
+    private String manufactory;
+
+    private int personnel;
+
+    private int standing;
+
+    private double salary;
+
+
+    static class Builder {
+        private String name;
+        private String gender;
+        private byte age;
+        private String manufactory;
+        private int personnel;
+        private int standing;
+        private double salary;
+
+        Builder buildName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        Builder buildGender(String gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        Builder buildAge(byte age) {
+            this.age = age;
+            return this;
+        }
+
+        Builder buildManufactory(String manufactory) {
+            this.manufactory = manufactory;
+            return this;
+        }
+
+        Builder buildPersonnel(int personnel) {
+            this.personnel = personnel;
+            return this;
+        }
+
+        Builder buildStanding(int standing) {
+            this.standing = standing;
+            return this;
+        }
+
+        Builder buildSalary(double salary) {
+            this.salary = salary;
+            return this;
+        }
+
+        Worker build() {
+            Worker worker = new Worker();
+            worker.name = name;
+            worker.gender = gender;
+            worker.age = age;
+            worker.manufactory = manufactory;
+            worker.personnel = personnel;
+            worker.standing = standing;
+            worker.salary = salary;
+            return worker;
+        }
+
+        public static void main(String[] args) {
+            Worker worker = new Builder().buildName("Igor")
+                    .buildGender("male")
+                    .buildAge((byte) 27)
+                    .buildManufactory("KINEF")
+                    .buildPersonnel(12039)
+                    .buildStanding(2)
+                    .buildSalary(28)
+                    .build();
+            System.out.println(worker);
+        }
+    }
+}
